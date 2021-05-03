e1a3d47bd046ecf501af77c3bad2428dc4f18655
monj021 <monj021@gmail.com>
2021-05-03 15:27:46 -0400

    main.js uploaded
    First upload of main.js
    
    Description: File to read an array of integers from the command line and convert them into an array of Strings representing the phonetic equivalent of the integers.
    
    For example, for a given integer array of the form [12, 345, 67],  the string that is printed out is "OneTwo, ThreeFourFive, SixSeven"
    
    Functions used
    
    1. Function Name: stringifyNumber
    Number of Arguments: 1
    Type of Arguments: Integer
    Description: Function to convert an integer into its Phonetic equivalent
    
    2. Function Name: getIntegerList
    Number of Arguments: 1
    Type of Arguments: Command Line Argument List
    Description: Function to convert command line arguments into integers and return as a list of integers 
    
    3. Function Name: getStringArray
    Number of Arguments: 1
    Type of Arguments: Integer Array
    Description: Function to convert a list of integers into a string array of their phonetic equivalent
    
    4. Function Name: printStringArray
    Number of Arguments: 1
    Type of Arguments: String
    Description: Function to print the input string
    
    5. Function Name: main
    Number of Arguments: 0
    Description: Trigger function that calls all other functions

---
+++ main.js
@@ -0,0 +1,60 @@
+var myArgs = process.argv;
+myString="";
+var digitToString = ["Zero", "One", "Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+prefix="";
+
+
+/* Function to converts an integer into its Phonetic equivalent*/
+function stringifyNumber(number) {
+
+        var stringifiedNumber = "";
+        while(number>0) {
+                var remainder = number%10;
+                var digitString = digitToString[remainder];
+                stringifiedNumber = digitString+stringifiedNumber;
+                number = number/10 | 0;
+        }
+        return stringifiedNumber;
+
+}
+
+
+/* Function to convert command line arguments into integers and return as a list of integers */
+function getIntegerList(myArgs)
+{
+        integerList=[];
+        for (i=2;i<myArgs.length;i++) {
+                integerList.push(parseInt(myArgs[i]));
+        }
+        return integerList;
+}
+
+/* Function to convert a list of integers into string array of their phonetic equivalent */
+function getStringArray(integerList) {
+        stringArray="";
+        for (i=0;i<integerList.length;i++) {
+                str=stringifyNumber(integerList[i]);
+                stringArray+=prefix+str;
+                prefix=", ";
+
+        }
+        return stringArray;
+} 
+
+/* Function to print the input string */
+function printStringArray(outputString)
+{
+       console.log(outputString); 
+}
+
+
+
+/* Main function that calls all the other functions */
+function main()
+{
+        integerList=getIntegerList(myArgs);
+        stringArray=getStringArray(integerList);
+        printStringArray(stringArray);
+}
+
+main();
